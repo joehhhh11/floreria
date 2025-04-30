a00c94e52776f569d6961d2990af77dcf537a648
MCRLogPose <cruzriveraedwinmanuel@gmail.com>
2025-04-29 23:39:15 -0500

    Mi primer commit del proyecto

---
+++ src/main/java/com/sigis/SigisAppApplication.java
@@ -0,0 +1,14 @@
+package com.sigis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigisAppApplication.class, args);
+                System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/com/sigis/SigisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
